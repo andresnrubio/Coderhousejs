196b27b48574b432a0a689061e7dd515222959ba
Andrés Nicolás Rubio <87342325+andresnrubio@users.noreply.github.com>
2022-08-24 16:45:57 -0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,7 @@ function calc_promedio() {
   var n2 = parseInt(document.getElementById("nota2").value);
   var n3 = parseInt(document.getElementById("nota3").value);
   let p = (n1 + n2 + n3) / 3;
-  p= p.toFixed(1)
+  p = p.toFixed(1);
   document.getElementById("promedio").innerText = "Su promedio es de " + p;
   return p;
 }
@@ -17,7 +17,6 @@ let array = [];
 for (let i = 1; i <= 10; i++) {
   array.push(getRandomInt(1, 10));
 }
-console.log(array);
 
 const nota1 = document.getElementById("nota1");
 nota1.addEventListener("keydown", (e) => {
@@ -34,7 +33,11 @@ nota3.addEventListener("keydown", (e) => {
   console.log("La tercer nota es:" + e.key);
 });
 
-const notas = { nota1: "n1", nota2: "n2", nota3: "n3" };
+const notas = {
+  nota1: "n1",
+  nota2: "n2",
+  nota3: "n3",
+};
 JSON.stringify(notas);
 
 const boton = document.getElementById("btn");
@@ -46,7 +49,7 @@ boton.onclick = () => {
       title: "Felicidades",
       text: "Aprobado con " + resultado,
     });
-    }else {
+  } else {
     Swal.fire({
       icon: "error",
       title: "Lo sentimos",
@@ -57,3 +60,28 @@ boton.onclick = () => {
 
 sessionStorage.setItem("calculo", JSON.stringify(notas));
 localStorage.setItem("calculo1", JSON.stringify(notas));
+
+
+const clima = document.getElementById("clima");
+let icon = "clear";
+
+const climaInfo = async () => {
+  const resp = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?lat=-34.603719&lon=-58.381834&lang=es&appid=6e06ac3c98dba32312d10ffe9a1499b2"
+  );
+  const data = await resp.json();
+  return data;
+};
+
+climaInfo().then((data) => {
+  const icon = data.weather[0].main.toLowerCase()
+
+  clima.innerHTML += `
+<img src="./icons/${icon}.svg" alt="">
+<div>
+<h1>13°</h1>
+<p>${data.name}, ${data.sys.country}</p>
+</div>`;
+});
+
+
